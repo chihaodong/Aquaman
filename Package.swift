777dd0428e64d6a9dc90229b42891647b7d6d12d
chihaodong <543903123@qq.com>
2020-07-06 13:23:45 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,9 +6,6 @@ let package = Package(
     products: [
         .library(name: "Aquaman", targets: ["Aquaman"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/chihaodong/Aquaman")
-    ],
     targets: [
         .target(name: "Aquaman", dependencies: ["Aquaman"], path: "Aquaman/Aquaman")
     ],
